09365b2e5b5e6eb5dbbee757c5574a41358e88a5
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2021-09-14 12:41:11 +0200

    subscriptions first commit

---
+++ subscriptions/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import "express-async-errors";
+import { json } from "body-parser";
+
+const app = express();
+app.use(json());
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3001, () => {
+    console.log("Listening on port 3001!");
+  });
+};
+
+start();
